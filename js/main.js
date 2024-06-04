85584283d4117139cb7584a80495b30072148b29
Youu99 <154948743+Youu99@users.noreply.github.com>
2024-06-04 15:53:03 +0900

    2024-08-04

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// 타이틀 타이핑
+
 const texts = ["WELCOME", "YOUMIN", "PORTFOLIO"];
 let index = 0;
 let charIndex = 0;
@@ -50,5 +52,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
   container.addEventListener('mouseleave', function () {
     container.classList.remove('flipped');
+  });
+});
+
+
+
+// 바텀 메뉴 체인지
+document.addEventListener("DOMContentLoaded", function() {
+  // 'hea_u' 아이디를 가진 요소의 모든 'li' 자식 요소들을 찾습니다.
+  var buttons = document.querySelectorAll('#hea_u li');
+  
+  buttons.forEach(function(button, index) {
+    button.addEventListener('click', function() {
+      // 모든 섹션을 숨깁니다.
+      document.querySelectorAll('.sc').forEach(function(section) {
+        section.style.display = 'none';
+      });
+      
+      // 클릭한 버튼에 해당하는 섹션만 보여줍니다. (인덱스는 0부터 시작하므로 1을 더합니다)
+      var sectionToShow = document.querySelector('.sc' + (index + 1));
+      if(sectionToShow) {
+        sectionToShow.style.display = 'block';
+      }
+    });
   });
 });
